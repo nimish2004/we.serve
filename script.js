87ed33c145e5daa2eb39d0845c2cdedf7a7ce05e
nimish2004 <123116200+nimish2004@users.noreply.github.com>
2024-01-02 21:37:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showDonationForm() {
+    var donationForm = document.getElementById('donationForm');
+    donationForm.style.display = (donationForm.style.display === 'none' || donationForm.style.display === '') ? 'block' : 'none';
+}
